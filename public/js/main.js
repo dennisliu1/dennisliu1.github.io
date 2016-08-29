bbdd91038d870a5503d44f0425ff02b7aeea4722
Dennis Liu <dennisliu12@gmail.com>
2016-08-28 22:38:05 -0400

    updated website with new design
    - updated font
    - updated links
    - redone layout with bootstrap (gave up on making it myself :( )

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,15 @@
 
 
 
-$(".item-name").click(function() {
-  $(this).siblings(".item-points").toggle();
-});
-
-$(".item-points-name").click(function() {
-  $(this).siblings(".item-points-descr").toggle();
-});
+// $(".item-name").click(function() {
+//   $(this).siblings(".item-points").toggle();
+// });
+//
+// $(".item-points-name").click(function() {
+//   $(this).siblings(".item-points-descr").toggle();
+// });
+//
+// $(".expandImg").css({
+//   "width": ((1280*0.75) * 0.95)+"px",
+//   "height": ((800*0.75) * 0.92)+"px"
+// });
